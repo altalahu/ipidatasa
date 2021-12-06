abf3f76d849e86da07178751d7a27f101af8e7fa
altalahu <95630713+altalahu@users.noreply.github.com>
2021-12-06 11:12:03 -0500

    Update my_first.py

--- my_first.py
+++ my_first.py
@@ -1,23 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pyodbc
-
 st.title("My First Web App")
 
-st.header("Temperatura de Cocinas")
-conn = pyodbc.connect('DRIVER={SQL Server};'
-                     'SERVER=192.168.31.207;'
-                     'Database=TASA;'
-                     'UID=rmUser;'
-                     'PWD=rmUser;'
-                     'Authentication=ActiveDirectoryPassword'
-                     'Trusted_Connection=yes;'
-                     )
-cursor = con.cursor()
-sqlquery2 = ''' select top 600 *
-from remotedb r
-where id >919536641 and [_NAME] like '%Cocinas Prensas.TT2%'
-and [_NAME] like '%P11%' '''
 
-ft_temperaturas = pd.read_sql(sqlquery2,conn)
-st.write(ft_temperaturas)
